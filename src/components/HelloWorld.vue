d79fbe42a3280f3620ad2cf9f2b634d96be79852
SHK <bedrook@gmail.com>
2019-05-08 06:58:22 +0900

    time timer vue electron porting... (basic setting)

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div id="timerContainer">
+      <div id="timerDiskHole"></div>
+      <div id="timerDisk"></div>
+      <div id="timerGroundEnd"></div>
+      <div id="timerBarEnd"></div>
+      <div id="timerBarKnob"></div>
+      <div id="timerDirection"></div>
+      <div id="timerTime"></div>
+    </div>
+    <button type="button" class="btn btn-primary" autocomplate="off" data-loading-text="jquery with bootstrap" @click="clickBtn"> Hello</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    
+  },
+  methods: {
+    clickBtn (event) {
+      $(event.target).button('loading')
+      console.log("click!")
+      
+      setTimeout(function() {
+        $(event.target).button('reset')
+      }, 1000);
+    }
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
